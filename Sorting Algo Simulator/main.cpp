4732952fdeda0091fac5a5ebf77cf3538b0c0a40
Md. Nafisur Rahman <nafis.cseaiub@gmail.com>
2021-04-01 12:16:18 +0600

    Update main.cpp

--- Sorting Algo Simulator/main.cpp
+++ Sorting Algo Simulator/main.cpp
@@ -11,31 +11,24 @@ void swap(int *a, int *b) {
 }
 
 int partition(int array[], int low, int high) {
-    // Select the pivot element
     int pivot = array[high];
     int i = (low - 1);
-    // Put the elements smaller than pivot on the left
-    // and greater than pivot on the right of pivot
+   
     for(int j = low; j < high; j++) {
         if(array[j] <= pivot) {
             i++;
             swap(&array[i], &array[j]);
         }
     }
-    printArray(array, 7);
-    cout << "........\n";
     swap(&array[i + 1], &array[high]);
     return (i + 1);
 }
 
 void quickSort(int array[], int low, int high) {
     if(low < high) {
-        // Select pivot position and put all the elements smaller
-        // than pivot on left and greater than pivot on right
+      
         int pi = partition(array, low, high);
-        // Sort the elements on the left of pivot
         quickSort(array, low, pi - 1);
-        // Sort the elements on the right of pivot
         quickSort(array, pi + 1, high);
     }
 }
