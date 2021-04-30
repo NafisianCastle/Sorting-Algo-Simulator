a3630cfb76a766a015afafd562b15d8528643b51
NafisianCastle <nafis.cseaiub@gmail.com>
2021-04-30 18:15:16 +0600

    modified

--- Sorting Algo Simulator/main.cpp
+++ Sorting Algo Simulator/main.cpp
@@ -1,110 +1,194 @@
-#include "GL/gl.h"
-#include <GL/glut.h>
-#include <unistd.h>
-#include <windows.h>
-#include <bits/stdc++.h>
 
-int length;
-int delay;
-int* arr;
-void (*Sort)(int*, int, int);
-int x=0.5;
-
-void renderFunction() {
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glClearColor(1.0, 1.0, 1.0, 0.0);
-    glLoadIdentity(); //Reset the drawing perspective
-    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-
-    float len = (float) length;
-    float widthAdder = 1.7 / len;
-    for(int i = 0; i < length; ++i) {
-        glBegin(GL_POLYGON);
-        float heightRatio = 1.5 * arr[i] / len;
-        float positionAdder = 3 * i / len;
-        float leftX   = -1 + positionAdder;
-        float rightX  = leftX + widthAdder;
-        float bottomY = -1;
-        float topY    = bottomY + heightRatio;
-        glColor3f(0,x, 0);
-        glVertex2f(leftX, bottomY);
-        glVertex2f(rightX, bottomY);
-        glVertex2f(rightX, topY);
-        glVertex2f(leftX, topY);
-        glEnd();
-        glutSwapBuffers();
-    }
-    glFlush();
+class insertionSort{
+public:
+  insertionSort(int* arr,int N);
+  void sort(int* arr,int N);
+  void exchange(int* arr,int i,int j);
+};
+insertionSort::insertionSort(int* arr,int N){
+  sort(arr,N);
+}
+void insertionSort::sort(int* arr,int N){
+  for(int i=1;i<N;i++)
+  {
+      for(int j=i;j>=1;j--)
+      {
+          compares++;
+          if(arr[j]<arr[j-1])
+              {
+                exchanges++;
+                exchange(arr,j,j-1);
+                visualization(arr);
+              }
+          else
+              break;
+      }
+  }
+}
+void insertionSort::exchange(int* arr,int i,int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
 
-void swap(int index1, int index2) {
-    glColor3f(1, 0, 0);
-    std::swap(arr[index1], arr[index2]);
-   // renderFunction();
-   glutPostRedisplay();
-    usleep(delay);
+class selectionSort{
+public:
+  selectionSort(int* arr,int N);
+  void sort(int* arr,int N);
+  void exchange(int* arr,int i,int j);
+};
+selectionSort::selectionSort(int* arr,int N){
+  sort(arr,N);
 }
-int partition(int* a, int low, int high) {
-    int lowIndex = low - 1, pivot    = a[high];
-    for(int i = low; i < high; ++i) {
-        if(a[i] <= pivot) {
-            ++lowIndex;
-            std::swap(a[lowIndex], a[i]);
-            //glutPostRedisplay();
+void selectionSort::sort(int* arr,int N)
+{
+    for(int i=0;i<N-1;i++)
+    {
+        int ind = i;
+        for(int j=i+1;j<N;j++)
+        {
+            compares++;
+            if(arr[j]<arr[ind])
+                ind = j;
         }
-    }
-    ++lowIndex;
-    std::swap(a[lowIndex], a[high]);
-    return lowIndex;
-}
-
-void quickSort(int* a, int low, int high) {
-    if(low < high) {
-        int pi = partition(a, low, high);
-        quickSort(a, low, pi - 1);
-        quickSort(a, pi + 1, high);
+        exchanges++;
+        exchange(arr,i,ind);
+        visualization(arr);
     }
 }
-
-void randomizeArray(int* arr, int length) {
-    for(int i = length - 1; i > 0; --i)
-        std::swap(arr[i], arr[rand() % (i + 1)]);
-}
-void keyboardEvent(unsigned char c, int x, int y) {
-    if(c == 27) {
-        // exit on escape key pressed
-        exit(0);
-        free(arr);
-    } else if(c == 's') {
-        // start on `s` key pressed
-        Sort(arr, 0, length - 1);
-    } else if(c == 'r')
-        //randomize on 'r' key pressed
-        randomizeArray(arr, length);
+void selectionSort::exchange(int* arr,int i,int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
 
-int drawScene(int argc, char** argv, void (*SortingAlgorithm)(int*, int, int)) {
-    Sort = SortingAlgorithm;
-    arr = (int*) malloc(sizeof(int) * length);
-    for(int i = 0; i < length; ++i)
-        arr[i] = i + 1;
-    randomizeArray(arr, length);
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(3 * length, length - (length  / 100));
-    glutInitWindowPosition(300, 200);
-    glutCreateWindow("Sort Algo Simulator");
-    glutDisplayFunc(renderFunction);
-    glutKeyboardFunc(keyboardEvent);
-    glutMainLoop();
+
+class quickSort{
+public:
+  quickSort(int* arr,int N);
+  void sort(int* arr,int lo,int hi);
+  void sort_shuffle(int* arr,int i,int j);
+  int part(int* arr,int lo,int hi);
+  void exchange(int* arr,int i,int j);
+};
+quickSort::quickSort(int* arr,int N){
+  sort_shuffle(arr,0,N);
+}
+void quickSort::sort_shuffle(int* arr,int i,int j)
+{
+  std::random_shuffle(arr,arr+j);
+  sort(arr,i,j);
+}
+void  quickSort::sort(int* arr,int lo,int hi)
+{
+  visualization(arr);
+  if(lo>=hi)
+  return;
+  int j = part(arr,lo,hi);
+  sort(arr,lo,j-1);
+  visualization(arr);
+  sort(arr,j+1,hi);
+  visualization(arr);
 }
 
-int main(int argc, char* argv[]) {
-    srand(time(NULL));
-    delay  = 1500;
-    length = 300;
-    drawScene(argc, argv, quickSort);
-    free(arr);
-    return 0;
+int quickSort::part(int* arr,int lo,int hi)
+{
+    int i = lo,j = hi+1;
+    while(true)
+    {
+        while(arr[++i]<arr[lo])
+        {
+          compares++;
+            if(i == hi)
+                break;
+        }
+        while(arr[lo]<arr[--j])
+        {
+          compares++;
+            if(j==lo)
+                break;
+        }
+        if(i>=j)
+            break;
+        exchanges++;
+        exchange(arr,i,j);
+        visualization(arr);
+    }
+    exchanges++;
+    exchange(arr,lo,j);
+    visualization(arr);
+    return j;
+}
+void quickSort::exchange(int* arr,int i,int j)
+{
+  int temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+class mergeSort{
+public:
+  mergeSort(int* arr,int N);
+  void sort(int* arr,int* aux,int lo,int hi);
+  void merge(int* arr,int* aux,int low,int mid, int high);
+};
+mergeSort::mergeSort(int* arr,int N){
+  int aux[N];
+  for(int i=0;i<N;i++){
+    aux[i] = arr[i];
+  }
+  sort(arr,aux,0,N-1);
+}
+void mergeSort::sort(int* arr,int* aux,int low,int high)
+{
+  compares++;
+  if(low>=high)
+  return;
+  int mid = low + (high-low)/2;
+  sort(arr,aux,low,mid);
+  visualization(arr);
+  sort(arr,aux,mid+1,high);
+  visualization(arr);
+  merge(arr,aux,low,mid,high);
+  visualization(arr);
+}
+void mergeSort::merge(int* arr,int* aux,int low,int mid, int high)
+{
+    int i=low,j=mid+1;
+    // This step can be optimized by alternatively using aux and arr array
+    // as the auxilary array
+    for(int i=0;i<N;i++)
+    {
+      aux[i] = arr[i];
+    }
+    for(int k=low;k<=high;k++)
+    {
+        if(i>mid)
+        {
+            arr[k] = aux[j++];
+            compares++;
+            exchanges++;
+        }
+        else if(j>high)
+        {
+            arr[k] = aux[i++];
+            compares++;
+            exchanges++;
+        }
+        else if(aux[i]>aux[j])
+        {
+            arr[k] = aux[j++];
+            compares++;
+            exchanges++;
+        }
+        else
+        {
+            arr[k] = aux[i++];
+            compares++;
+            exchanges++;
+        }
+        visualization(arr);
+    }
 }
